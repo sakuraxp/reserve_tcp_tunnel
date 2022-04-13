53c0e3e22e13c9044dc9a601bcea08573fdd408a
sakuraxp <sakura_xp@sina.com>
2022-04-14 02:03:07 +0800

    update

--- reserve_tcp_tunnel_client.py
+++ reserve_tcp_tunnel_client.py
@@ -1,24 +1,14 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
 import asyncio
 
-#import hexdump
 
-_CLIENT_CONNECTIONS = 1000
+class Reserve_TCP_Tunnel_Client:
+    def __init__(self, ts_config, hp_config):
+        self._ts_host, self._ts_port = ts_config
+        self._hp_host, self._hp_port = hp_config
 
-
-class Client:
-    '''
-    Reserve TCP tunnel client
-    '''
-    def __init__(self, tunnel_server_ip, tunnel_server_port, http_proxy_ip, http_proxy_port):
-        self.__tunnel_server_ip = tunnel_server_ip
-        self.__tunnel_server_port = tunnel_server_port
-        self.__http_proxy_ip = http_proxy_ip
-        self.__http_proxy_port = http_proxy_port
-
-    async def __copy_data_ptoa56(self, info, p_reader5, a_writer6):
+    async def _copy_data_pc56(self, info, p_reader5, a_writer6):
         addr = a_writer6.get_extra_info('peername')
         total = 0
         while True:
@@ -26,86 +16,64 @@ class Client:
             if not chunk:
                 break
             total += len(chunk)
-            print("{0}:{1}".format(info, addr))
-            #hexdump.hexdump(chunk[:32])
             a_writer6.write(chunk)
             await a_writer6.drain()
-        print('5->6 {} done. total={}'.format(info, total))
-        print("close sock[{0}]:{1}".format(info, addr))
+        print(f"close sock[{info}]:{addr}, total send={total}.")
         a_writer6.close()
-        asyncio.ensure_future(self.tunnel_server_connection(1))
+        asyncio.ensure_future(self.connect_tunnel_server())
 
-    async def __copy_data_atop34(self,
-                                 info,
-                                 a_reader3,
-                                 a_writer6,
-                                 p_reader5=None,
-                                 p_writer4=None):
+    async def _copy_data_cp34(self, info, a_reader3, a_writer6):
         addr = a_writer6.get_extra_info('peername')
         total = 0
         while True:
             chunk = await a_reader3.read(1024)
             if not chunk:
-                assert total != 0
                 break
-            total += len(chunk)
-            if p_reader5 == None:
-                p_reader5, p_writer4 = await self._connect(
-                    'http_proxy', self.__http_proxy_ip, self.__http_proxy_port)
+
+            if total == 0:
+                print(f"open sock[tc->hp]: {self._hp_host}:{self._hp_port}")
+                p_reader5, p_writer4 = await asyncio.open_connection(
+                    self._hp_host, self._hp_port)
                 asyncio.ensure_future(
-                    self.__copy_data_ptoa56('p->a', p_reader5, a_writer6))
-            print("{0}:{1}".format(info, addr))
-            #hexdump.hexdump(chunk[:32])
+                    self._copy_data_pc56('hp->tc', p_reader5, a_writer6))
+
+            total += len(chunk)
             p_writer4.write(chunk)
             await p_writer4.drain()
-        print('3->4 {} done. total={}'.format(info, total))
-        print("close sock[{0}]:{1}".format(info, addr))
+        print(f"close sock[{info}]:{addr}, total send={total}.")
         p_writer4.close()
 
-    async def _connect(self, info, ip, port):
-        print('try connection {} {}:{}'.format(info, ip, port))
-        reader, writer = await asyncio.open_connection(ip, port)
-        addr = writer.get_extra_info('peername')
-        print("connection to[{0}]:{1}".format(info, addr))
-        return reader, writer
-
-    async def tunnel_server_connection(self, id):
-        a_reader3, a_writer6 = await self._connect('tunnel_server',
-                                                   self.__tunnel_server_ip,
-                                                   self.__tunnel_server_port)
+    async def connect_tunnel_server(self):
+        a_reader3, a_writer6 = await asyncio.open_connection(
+            self._ts_host, self._ts_port)
         task = asyncio.ensure_future(
-            self.__copy_data_atop34('a->p',
-                                    a_reader3,
-                                    a_writer6,
-                                    p_reader5=None,
-                                    p_writer4=None))
-        print('create_tunnel_server_connection id = {}'.format(id))
+            self._copy_data_cp34('tc->hp', a_reader3, a_writer6))
 
+    def run(self, CONST_CLIENT_CONNECTIONS=500):
+        loop = asyncio.get_event_loop()
+        print(
+            f"open sock[tunnel_server]: {self._ts_host}:{self._ts_port}, CONNECTIONS = {CONST_CLIENT_CONNECTIONS}"
+        )
+        for _ in range(CONST_CLIENT_CONNECTIONS):
+            asyncio.ensure_future(client.connect_tunnel_server())
 
-def main(tunnel_server_ip, tunnel_server_port, http_proxy_ip, http_proxy_port):
-    tp = Client(tunnel_server_ip, tunnel_server_port, http_proxy_ip, http_proxy_port)
-    loop = asyncio.get_event_loop()
-    global _CLIENT_CONNECTIONS
-    for i in range(_CLIENT_CONNECTIONS):
-        asyncio.ensure_future(tp.tunnel_server_connection(i), loop=loop)
-    while True:
-        try:
-            loop.run_forever()
-        except ConnectionRefusedError as e:
-            print('Connection to server {} failed!'.format(e))
-        except asyncio.TimeoutError as e:
-            print('Connection to server {} timed out!'.format(e))
-        else:
-            print('Connection to server {} is closed.'.format(tp))
-        finally:
-            pass
-            asyncio.ensure_future(tp.tunnel_server_connection())
+        while True:
+            try:
+                loop.run_forever()
+            except ConnectionRefusedError as e:
+                print('Connection to server {} failed!'.format(e))
+            except asyncio.TimeoutError as e:
+                print('Connection to server {} timed out!'.format(e))
+            else:
+                print('Connection to server {} is closed.'.format(client))
+            finally:
+                pass
+                asyncio.ensure_future(client.connect_tunnel_server())
 
 
 if __name__ == "__main__":
-    server_ip = '127.0.0.1'
-    server_port = 2222
-    main(tunnel_server_ip=server_ip,
-         tunnel_server_port=server_port,
-         http_proxy_ip='127.0.0.1',
-         http_proxy_port=8118)
+    ts_config = '127.0.0.1', 2222
+    hp_config = '127.0.0.1', 8118
+    CONST__CLIENT_CONNECTIONS = 500
+    client = Reserve_TCP_Tunnel_Client(ts_config, hp_config)
+    client.run(CONST__CLIENT_CONNECTIONS)
--- reserve_tcp_tunnel_server.py
+++ reserve_tcp_tunnel_server.py
@@ -3,95 +3,63 @@
 
 import asyncio
 
-#import hexdump
 
-
-class Server:
-    '''
-    Reserve TCP tunnel server
-    '''
-    def __init__(self):
+class Reserve_TCP_Tunnel_Server:
+    def __init__(self, ts_config, lp_config):
+        self._ts_host, self._ts_port = ts_config
+        self._lp_host, self._lp_port = lp_config
         self._queue = asyncio.Queue()
-        self.__proxy_count = 0
 
-    async def __copy_data_ptoa12(self, info, p_reader1, p_writer2):
-        addr = p_writer2.get_extra_info('peername')
+    async def _copy_data(self, info, reader, writer):
+        addr = writer.get_extra_info('peername')
         total = 0
         while True:
-            chunk = await p_reader1.read(1024)
+            chunk = await reader.read(1024)
             if not chunk:
                 break
             total += len(chunk)
-            print("{0}:{1}".format(info, addr))
-            #hexdump.hexdump(chunk[:32])
-            p_writer2.write(chunk)
-            await p_writer2.drain()
-        print('1->2 {} done. total={}'.format(info, total))
-        p_writer2.close()
+            writer.write(chunk)
+            await writer.drain()
+        print(f"close sock[{info}]:{addr}, total send={total}.")
+        writer.close()
 
-    async def __copy_data_atop78(self, info, a_reader7, p_writer8):
-        addr = p_writer8.get_extra_info('peername')
-        total = 0
-        while True:
-            chunk = await a_reader7.read(1024)
-            print('7->8 {}. total={}'.format(info, total))
-            if not chunk:
-                break
-            total += len(chunk)
-            print("{0}:{1}".format(info, addr))
-            #hexdump.hexdump(chunk[:32])
-            p_writer8.write(chunk)
-            await p_writer8.drain()
-        print('7->8 {} done. total={}'.format(info, total))
-        print("close sock[{0}]:{1}".format(info, addr))
-        p_writer8.close()
+    async def _tunnel_handle(self, c_reader7, c_writer2):
+        '''处理 tunnel_client -> tunnel_server 的连接 '''
+        await self._queue.put([c_reader7, c_writer2])
+        addr = c_writer2.get_extra_info('peername')
+        print(f"open sock[c->s]:{addr}, connected:{self._queue.qsize()}")
 
-    async def tcp_tunnel_handle(self, a_reader7, a_writer2):
-        '''
-        处理tcp_tunnel_client -> tcp_tunnel_listener 的连接
-        '''
-        addr = a_writer2.get_extra_info('peername')
-        print("[tunnel_client]to[tunnel_server]:{}".format(addr))
-        await self._queue.put([a_reader7, a_writer2])
-        #a_reader7.read(4)
-        print("client_ready_connections count={}".format(self._queue.qsize()))
+    async def _proxy_handle(self, s_reader1, s_writer8):
+        '''处理 server 端本地的的代理请求'''
+        addr = s_writer8.get_extra_info('peername')
+        print(f"open sock[localproxy]: {addr}")
+        c_reader7, c_writer2 = await self._queue.get()
+        asyncio.ensure_future(self._copy_data('s->c', s_reader1, c_writer2))
+        asyncio.ensure_future(self._copy_data('c->s', c_reader7, s_writer8))
 
-    async def local_proxy_handle(self, p_reader1, p_writer8):
-        '''
-        处理 server 端本地的的代理请求
-        '''
-        self.__proxy_count += 1
-        addr = p_writer8.get_extra_info('peername')
-        print("sock[{}]count{}:{}".format('localproxy', self.__proxy_count,
-                                          addr))
-        a_reader7, a_writer2 = await self._queue.get()
-        asyncio.ensure_future(
-            self.__copy_data_ptoa12('p->a', p_reader1, a_writer2))
-        asyncio.ensure_future(
-            self.__copy_data_atop78('a->p', a_reader7, p_writer8))
-
-
-def main(tunnelListener, localProxyListener):
-    tp = Server()
-    loop = asyncio.get_event_loop()
-    coro = asyncio.start_server(tp.tcp_tunnel_handle, *tunnelListener)
-    server = loop.run_until_complete(coro)
-    print('Serving on tcp_tunnel{}'.format(server.sockets[0].getsockname()))
-    coro2 = asyncio.start_server(tp.local_proxy_handle, *localProxyListener)
-    server2 = loop.run_until_complete(coro2)
-    print('Serving on local_proxy{}'.format(server2.sockets[0].getsockname()))
-    try:
-        loop.run_forever()
-    except KeyboardInterrupt:
-        pass
-
-    server.close()
-    loop.run_until_complete(server.wait_closed())
-    server2.close()
-    loop.run_until_complete(server.wait_closed())
-    loop.close()
+    def run(self):
+        loop = asyncio.get_event_loop()
+        ts_coro = asyncio.start_server(self._tunnel_handle, self._ts_host,
+                                       self._ts_port)
+        ts_loop = loop.run_until_complete(ts_coro)
+        print('Serving for tunnel {}'.format(ts_loop.sockets[0].getsockname()))
+        lp_coro = asyncio.start_server(self._proxy_handle, self._lp_host,
+                                       self._lp_port)
+        lp_loop = loop.run_until_complete(lp_coro)
+        print('Serving on localProxy {}'.format(
+            lp_loop.sockets[0].getsockname()))
+        try:
+            loop.run_forever()
+        except KeyboardInterrupt:
+            pass
+        ts_loop.close()
+        loop.run_until_complete(ts_loop.wait_closed())
+        lp_loop.close()
+        loop.run_until_complete(lp_loop.wait_closed())
+        loop.close()
 
 
 if __name__ == "__main__":
-    main(tunnelListener=('0.0.0.0', 2222),
-         localProxyListener=('0.0.0.0', 8080))
+    ts = Reserve_TCP_Tunnel_Server(ts_config=('0.0.0.0', 2222),
+                                   lp_config=('0.0.0.0', 8080))
+    ts.run()
